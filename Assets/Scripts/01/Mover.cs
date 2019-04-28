cdb95861d3328549fe995f76ce30a98d85683ee2
david <david.limkys@gmail.com>
2019-04-28 04:14:12 +0300

    changed layout

---
+++ Assets/Scripts/01/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace _01
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField]
+        private int speed = 1;
+    
+        void Update()
+        {
+            var position = transform.position;
+            position = new Vector3(position.x + speed * Time.deltaTime, position.y, position.z);
+       
+            transform.position = position;
+        }
+    }
+}
